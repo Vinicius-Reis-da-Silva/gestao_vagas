9828a6472c142c68f9b98b7e09653d2d49bcd6d0
Vinicius-Reis-da-Silva <viniciusreissilva808@gmail.com>
2023-12-14 17:40:41 -0300

    Inserting Spring Data JPA and starting the database

--- src/main/java/br/com/viniciusreis/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/viniciusreis/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -1,9 +1,14 @@
 package br.com.viniciusreis.gestao_vagas.modules.candidate;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
@@ -11,7 +16,10 @@ import lombok.Data;
 @Data
 public class CandidateEntity {
   
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
   private UUID id;
+
   private String name;
 
   @Pattern(regexp = "\\S+", message = "O campo [username] não deve conter espaça em branco")
@@ -24,5 +32,7 @@ public class CandidateEntity {
   private String password;
   private String description;
   private String curriculum;
- 
+  
+  @CreationTimestamp
+  private LocalDateTime createdAt;
 }
