790c66b4ac40c4ed192edca2d9f0625511543448
Vinicius-Reis-da-Silva <viniciusreissilva808@gmail.com>
2023-12-21 15:45:39 -0300

    Secure JWT recovery with expiration, Spring Security and access filtering. Token sent in requests.

--- src/main/java/br/com/viniciusreis/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
+++ src/main/java/br/com/viniciusreis/gestao_vagas/modules/company/useCases/AuthCompanyUseCase.java
@@ -1,5 +1,8 @@
 package br.com.viniciusreis.gestao_vagas.modules.company.useCases;
 
+import java.time.Duration;
+import java.time.Instant;
+
 import javax.security.sasl.AuthenticationException;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -44,6 +47,7 @@ public class AuthCompanyUseCase {
     // Se for igual -> Gerar o token
     Algorithm algorithm = Algorithm.HMAC256(secretKey);
     var token = JWT.create().withIssuer("javagas")
+                  .withExpiresAt(Instant.now().plus(Duration.ofHours(2)))
                   .withSubject(company.getId().toString())
                   .sign(algorithm);
     return token;
