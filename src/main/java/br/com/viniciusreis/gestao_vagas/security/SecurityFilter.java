eca514c77b656d1d4f53af8fc76e9d7f14b5e164
Vinicius-Reis-da-Silva <viniciusreissilva808@gmail.com>
2024-01-10 16:14:58 -0300

    Exploration of tokens and security filters in Spring Boot for authentication and authorization. Modifications ensure correct execution of filters. Left to do: adding roles and continuous configuration.

--- src/main/java/br/com/viniciusreis/gestao_vagas/security/SecurityFilter.java
+++ src/main/java/br/com/viniciusreis/gestao_vagas/security/SecurityFilter.java
@@ -30,19 +30,21 @@ public class SecurityFilter extends OncePerRequestFilter{
     SecurityContextHolder.getContext().setAuthentication(null);
     String header = request.getHeader("Authorization");
 
-    System.out.println(header);
+    if (request.getRequestURI().startsWith("/company")) {
+      if(header != null) {
+        var subjectToken = this.jwtProvider.validateToken(header);
 
-    if(header != null) {
-      var subjectToken = this.jwtProvider.validateToken(header);
-      if (subjectToken.isEmpty()) {
-        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-        return;
+        if (subjectToken.isEmpty()) {
+          response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+          return;
+        }
+        
+        request.setAttribute("company_id", subjectToken);
+        UsernamePasswordAuthenticationToken auth = 
+        new UsernamePasswordAuthenticationToken(subjectToken, null, Collections.emptyList());
+
+        SecurityContextHolder.getContext().setAuthentication(auth);
       }
-      
-      request.setAttribute("company_id", subjectToken);
-      UsernamePasswordAuthenticationToken auth = 
-      new UsernamePasswordAuthenticationToken(subjectToken, null, Collections.emptyList());
-      SecurityContextHolder.getContext().setAuthentication(auth);
     }
     
     filterChain.doFilter(request, response);
